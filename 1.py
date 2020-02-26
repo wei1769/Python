89dddb080cedfcb609b6f97eea39784a7efa96bb
williamlin910403 <williamlin910403@gmail.com>
2020-02-26 19:13:14 +0800

    correct UTF-8

--- 1.py
+++ 1.py
@@ -1,3 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+
 import requests
 
-res = requests.get('')
+
+res = requests.get('https://resource.hle.com.tw/Books/BooksResource/03_基礎化學(三)課本_第1章 氣體(108f814086).pdf')
+
+print (res)
