1d9822cfb6f71f16095b6646000b2104f7bd3f21
williamlin910403 <williamlin910403@gmail.com>
2020-02-26 20:53:52 +0800

    test request function

--- 1.py
+++ 1.py
@@ -3,8 +3,19 @@
 
 
 import requests
+head ='https://resource.hle.com.tw/Books/BooksResource/'
+end  = ').pdf'
+name ='03_基礎化學(三)課本_第1章 氣體(108f81'
+number = 4080
 
+urll = head + name + str(number) + end
+res = requests.get(urll)
+#res = requests.get('https://resource.hle.com.tw/Books/BooksResource/03_基礎化學(三)課本_第1章 氣體(108f814086).pdf')
+#res1 = requests.get('https://resource.hle.com.tw/Books/BooksResource/03_基礎化學(三)課本_第1章 氣體(108f814086).pdf')
 
-res = requests.get('https://resource.hle.com.tw/Books/BooksResource/03_基礎化學(三)課本_第1章 氣體(108f814086).pdf')
-
-print (res)
+pp = "aa"
+pp = res.status_code
+if pp == 200 :
+    print ("aa")
+else :
+    print (pp)
