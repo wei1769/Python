c480ac306ec20a52280b8d6aeb85132aaab7f60a
williamlin910403 <williamlin910403@gmail.com>
2020-02-26 22:13:03 +0800

    beta v0.3

--- 1.py
+++ 1.py
@@ -6,7 +6,7 @@ import requests
 head ='https://resource.hle.com.tw/Books/BooksResource/'
 end  = ').pdf'
 name ='02_選修物理(上)互動式教學講義_CH1(108f8'
-number = 13000
+number = 10000
 url = head + name + str(number) + end
 res = requests.get(url)
 response = res.status_code
