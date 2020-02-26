03502293b6b5279643f2880dbe8a50a5ee4b32bc
williamlin910403 <williamlin910403@gmail.com>
2020-02-26 21:26:09 +0800

    beta v0.1

--- 1.py
+++ 1.py
@@ -5,17 +5,18 @@
 import requests
 head ='https://resource.hle.com.tw/Books/BooksResource/'
 end  = ').pdf'
-name ='03_基礎化學(三)課本_第1章 氣體(108f81'
-number = 4080
-
-urll = head + name + str(number) + end
-res = requests.get(urll)
+name ='01_選修物理(下)互動式教學講義_編輯大意、目次(108f8'
+number = 14000
+url = head + name + str(number) + end
+res = requests.get(url)
+response = res.status_code
 #res = requests.get('https://resource.hle.com.tw/Books/BooksResource/03_基礎化學(三)課本_第1章 氣體(108f814086).pdf')
 #res1 = requests.get('https://resource.hle.com.tw/Books/BooksResource/03_基礎化學(三)課本_第1章 氣體(108f814086).pdf')
-
-pp = "aa"
-pp = res.status_code
-if pp == 200 :
-    print ("aa")
-else :
-    print (pp)
+while response != 200 :
+    print(response)
+    print(url)
+    number = number + 1
+    url = head + name + str(number) + end
+    res = requests.get(url)
+    response = res.status_code
+print (url)
